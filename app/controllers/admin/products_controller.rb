ca61b5f6166738f099d35c504e9018e84c3e1571
vagrant <t.oleary@me.com>
2021-09-17 04:20:37 +0000

    finalize admin auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['USERNAME'],password: ENV['PASSWORD']
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'],password: ENV['AUTH_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
